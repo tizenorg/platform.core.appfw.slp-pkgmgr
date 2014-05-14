f2648f02e46a8c3dbf209d8341e20379443744f6
jooseong.lee <jooseong.lee@samsung.com>
2014-05-14 16:44:57 +0900

    Modification about smack label of db file belong to process.
    Change-Id: Icf6072c76f517486d2375d0bc1c73afe6a1a3bd1
    Signed-off-by: jooseong.lee <jooseong.lee@samsung.com>

--- tool/pkg_initdb.c
+++ tool/pkg_initdb.c
@@ -266,17 +266,14 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-/*
-	const char *argv_parser[] = { "/usr/bin/chsmack", "-a", PKG_INFO_DB_LABEL, PKG_PARSER_DB_FILE, NULL };
+	const char *argv_parser[] = { "/usr/bin/chsmack", "-a", "User", PKG_PARSER_DB_FILE, NULL };
 	initdb_xsystem(argv_parser);
-	const char *argv_parserjn[] = { "/usr/bin/chsmack", "-a", PKG_INFO_DB_LABEL, PKG_PARSER_DB_FILE_JOURNAL, NULL };
+	const char *argv_parserjn[] = { "/usr/bin/chsmack", "-a", "User", PKG_PARSER_DB_FILE_JOURNAL, NULL };
 	initdb_xsystem(argv_parserjn);
-	const char *argv_cert[] = { "/usr/bin/chsmack", "-a", PKG_INFO_DB_LABEL, PKG_CERT_DB_FILE, NULL };
+	const char *argv_cert[] = { "/usr/bin/chsmack", "-a", "User", PKG_CERT_DB_FILE, NULL };
 	initdb_xsystem(argv_cert);
-	const char *argv_certjn[] = { "/usr/bin/chsmack", "-a", PKG_INFO_DB_LABEL, PKG_CERT_DB_FILE_JOURNAL, NULL };
+	const char *argv_certjn[] = { "/usr/bin/chsmack", "-a", "User", PKG_CERT_DB_FILE_JOURNAL, NULL };
 	initdb_xsystem(argv_certjn);
-*/
+
 	return 0;
 }
-
-
