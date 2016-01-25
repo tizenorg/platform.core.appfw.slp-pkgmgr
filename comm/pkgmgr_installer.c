749c9fbb5c24950aa680a08e0bc491e79c9d788c
Junghyun Yeon <jungh.yeon@samsung.com>
2016-01-25 15:15:11 +0900

    Implement app signal related with app disable/enable
    Changes applied at
    [pkgmgr-tool][pkgmgr-info][slp-pkgmgr][pkgmgr-server]
    
    Change-Id: I0e7af16a23329fd4f364e6d5932e8bc24d43ef0a
    Signed-off-by: Junghyun Yeon <jungh.yeon@samsung.com>

--- comm/pkgmgr_installer.c
+++ comm/pkgmgr_installer.c
@@ -81,11 +81,47 @@ static const char *__get_signal_name(pkgmgr_installer *pi, const char *key)
 		return COMM_STATUS_BROADCAST_EVENT_UPGRADE;
 	case PKGMGR_REQ_MOVE:
 		return COMM_STATUS_BROADCAST_EVENT_MOVE;
+	case PKGMGR_REQ_ENABLE_DISABLE_APP:
+		return COMM_STATUS_BROADCAST_EVENT_ENABLE_DISABLE_APP;
 	}
 
 	ERR("cannot find type, send signal with type SIGNAL_STATUS");
 
 	return COMM_STATUS_BROADCAST_SIGNAL_STATUS;
+}
+
+static int __send_signal_for_app_event(pkgmgr_installer *pi, const char *pkg_type,
+		const char *pkgid, const char *appid, const char *key, const char *val)
+{
+	char *sid;
+	const char *name;
+	GError *err = NULL;
+
+	if (!pi || pi->conn == NULL || appid == NULL)
+		return -1;
+
+	sid = pi->session_id;
+	if (!sid)
+		sid = "";
+
+	name = __get_signal_name(pi, key);
+	if (name == NULL) {
+		ERR("unknown signal type");
+		return -1;
+	}
+
+	if (g_dbus_connection_emit_signal(pi->conn, NULL,
+				COMM_STATUS_BROADCAST_OBJECT_PATH,
+				COMM_STATUS_BROADCAST_INTERFACE, name,
+				g_variant_new("(ussssss)", getuid(), sid,
+					pkg_type, pkgid, appid, key, val), &err)
+			!= TRUE) {
+		ERR("failed to send dbus signal: %s", err->message);
+		g_error_free(err);
+		return -1;
+	}
+
+	return 0;
 }
 
 static int __send_signal_for_event(pkgmgr_installer *pi, const char *pkg_type,
@@ -111,8 +147,8 @@ static int __send_signal_for_event(pkgmgr_installer *pi, const char *pkg_type,
 	if (g_dbus_connection_emit_signal(pi->conn, NULL,
 				COMM_STATUS_BROADCAST_OBJECT_PATH,
 				COMM_STATUS_BROADCAST_INTERFACE, name,
-				g_variant_new("(usssss)", getuid(), sid,
-					pkg_type, pkgid, key, val), &err)
+				g_variant_new("(ussssss)", getuid(), sid,
+					pkg_type, pkgid, "", key, val), &err)
 			!= TRUE) {
 		ERR("failed to send dbus signal: %s", err->message);
 		g_error_free(err);
@@ -433,6 +469,35 @@ API int pkgmgr_installer_send_app_uninstall_signal(pkgmgr_installer *pi,
 }
 
 API int
+pkgmgr_installer_send_app_signal(pkgmgr_installer *pi,
+			     const char *pkg_type,
+			     const char *pkgid,
+			     const char *appid,
+			     const char *key, const char *val)
+{
+	int r = 0;
+	char *sid;
+
+	if (!pi->conn) {
+		ERR("connection is NULL");
+		return -1;
+	}
+
+	sid = pi->session_id;
+	if (!sid) {
+		ERR("session id is NULL, set empty string");
+		sid = "";
+	}
+
+	if (strcmp(key, PKGMGR_INSTALLER_UPGRADE_EVENT_STR) == 0)
+		pi->request_type = PKGMGR_REQ_UPGRADE;
+
+	r = __send_signal_for_app_event(pi, pkg_type, pkgid, appid, key, val);
+
+	return r;
+}
+
+API int
 pkgmgr_installer_send_signal(pkgmgr_installer *pi,
 			     const char *pkg_type,
 			     const char *pkgid,
@@ -451,6 +516,24 @@ pkgmgr_installer_send_signal(pkgmgr_installer *pi,
 	r = __send_signal_for_event(pi, pkg_type, pkgid, key, val);
 
 	return r;
+}
+
+API int pkgmgr_installer_set_request_type(pkgmgr_installer *pi, int request_type)
+{
+	if (pi == NULL)
+		return -1;
+
+	pi->request_type = request_type;
+	return 0;
+}
+
+API int pkgmgr_installer_set_session_id(pkgmgr_installer *pi, char *session_id)
+{
+	if (pi == NULL || session_id == NULL)
+		return -1;
+
+	pi->session_id = strndup(session_id, MAX_STRLEN);
+	return 0;
 }
 
 API int pkgmgr_installer_create_certinfo_set_handle(pkgmgr_instcertinfo_h *handle)
