a72191890ce158fe75f54b5395c706f28dfcedd3
Junghyun Yeon <jungh.yeon@samsung.com>
2016-03-11 20:46:18 +0900

    - change enum value of COMM_STATUS_BROADCAST_ALL to cover enable/disable of app - remove useless code
    Change-Id: I7831215bbb64a55c116126606109aed8dd17a6b1
    Signed-off-by: Junghyun Yeon <jungh.yeon@samsung.com>

--- comm/pkgmgr_installer.c
+++ comm/pkgmgr_installer.c
@@ -503,9 +503,6 @@ pkgmgr_installer_send_app_signal(pkgmgr_installer *pi,
 		return -1;
 	}
 
-	if (strcmp(key, PKGMGR_INSTALLER_UPGRADE_EVENT_STR) == 0)
-		pi->request_type = PKGMGR_REQ_UPGRADE;
-
 	r = __send_signal_for_app_event(pi, pkg_type, pkgid, appid, key, val);
 
 	return r;
